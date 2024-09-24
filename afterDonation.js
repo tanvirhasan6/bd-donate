effce3c6456eda7df8e1cbbc37cb5c9d06ef93fe
tanvirhasan6 <tanvir.hasan6@gmail.com>
2024-09-24 09:44:41 +0600

    final save

--- afterDonation.js
+++ afterDonation.js
@@ -8,25 +8,26 @@ function donationCalculation(donationId,donatedId,donationHeadID){
     const donatedAmt = parseFloat(document.getElementById(donatedId).innerHTML);
 
     const donationTxt = document.getElementById(donationHeadID).innerHTML;
+    const donationAmount = parseFloat(donationAmt);
 
     if(isNaN(donationAmt) || !donationAmt){
         alert('Caution!! Inputed value is not a number!');
-    }else if(parseFloat(donationAmt)<=0){
+    }else if(donationAmount<=0){
         alert('Sorry, Your amount is too low!');
-    }else if(parseFloat(donationAmt)>balance){
+    }else if(donationAmount>balance){
         alert('Sorry, Your balance is not enough!');
     }
     else{
-
-        balanceElement.innerHTML = balance - parseFloat(donationAmt);
-        donationElement.innerHTML = parseFloat(donationAmt) + donatedAmt;
+        
+        balanceElement.innerHTML = balance - donationAmount;
+        donationElement.innerHTML = donationAmount + donatedAmt;
 
         const historydiv = document.createElement('div');
         historydiv.className = 'w-full p-4 flex flex-col justify-center items-start border rounded-lg';
 
         const newP = document.createElement('p');
         newP.className = 'text-sm font-bold mb-5';
-        newP.textContent = parseFloat(donationAmt) + ' Tk. Donated for ' + donationTxt;
+        newP.textContent = donationAmount + ' Tk. Donated for ' + donationTxt;
 
         const dateP = document.createElement('p');
         dateP.className = 'text-sm ml-2';
@@ -38,10 +39,14 @@ function donationCalculation(donationId,donatedId,donationHeadID){
         historydiv.appendChild(dateP);
 
         parentDiv.appendChild(historydiv);
-        
+
+        const modalMsgElement = document.getElementById('modal-success-amount');        
+        modalMsgElement.innerHTML = donationAmount + ' Tk Successfully.';
 
         const modal = document.getElementById('donate-modal');
         modal.showModal(); 
+
+        document.getElementById(donationId).value = '';
     }
     
     
